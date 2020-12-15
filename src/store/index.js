745502a402bd50c5daaa2773e183d0039c8c3eb8
Luke Olali <mugapedia@gmail.com>
2020-12-15 17:28:14 +0300

    fixes typo in the name of the window.__REDUX_DEVTOOLS_EXTENSION__

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import rootReducer from '../reducers'
 const configureStore = () => {
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION_ && window.__REDUX_DEVTOOLS_EXTENSION_()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 }
 
