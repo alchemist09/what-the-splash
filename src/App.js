21949b07ff8014218e374f42a7142f97d8e7e395
Luke Olali <mugapedia@gmail.com>
2020-12-15 17:44:04 +0300

    change the configureStore from named import to default import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { configureStore } from './store';
+import configureStore from './store';
 
 import Header from './components/Header';
 import ImageGrid from './components/ImageGrid';
