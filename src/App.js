14695e275cc1c08e052ae3612010e0553346ca94
Luke Olali <mugapedia@gmail.com>
2020-12-15 17:38:26 +0300

    moves the function call to create the store below the import statements for components to prevent compile time errors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 
-const store = configureStore()
-
 import Header from './components/Header';
 import ImageGrid from './components/ImageGrid';
+
+const store = configureStore()
 
 class App extends Component {
     render() {
