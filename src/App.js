2ce24658e5da5f78934600c5ddecc5bdadaf3adc
Luke Olali <mugapedia@gmail.com>
2020-12-15 17:26:10 +0300

    injects the store at the top level of the component tree to make it available throughout the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
+
+const store = configureStore()
 
 import Header from './components/Header';
 import ImageGrid from './components/ImageGrid';
@@ -6,10 +10,12 @@ import ImageGrid from './components/ImageGrid';
 class App extends Component {
     render() {
         return (
-            <div>
-                <Header />
-                <ImageGrid />
-            </div>
+            <Provider store={store}>
+                <div>
+                    <Header />
+                    <ImageGrid />
+                </div>
+            </Provider>
         );
     }
 }
