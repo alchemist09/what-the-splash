8a691ecaa806226f1082e330375ae92ff2171ce9
Luke Olali <mugapedia@gmail.com>
2020-12-16 16:41:33 +0300

    removes the local component state and image loading logic from componentDidMount()

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -7,23 +7,12 @@ import './styles.css';
 const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
 
 class ImageGrid extends Component {
-    state = {
-        images: [],
-    };
 
     componentDidMount() {
-        fetch(`https://api.unsplash.com/photos/?client_id=${key}&per_page=28`)
-            .then(res => res.json())
-            .then(images => {
-                this.setState({
-                    images,
-                });
-            });
-        console.log(this.props)
+        
     }
 
     render() {
-        // const { images } = this.state;
         const { loadImages, images, error } = this.props
         return (
             <div className="content">
