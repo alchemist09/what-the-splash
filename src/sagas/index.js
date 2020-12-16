5aa9b3e90a56693c5610f8d935a00c3b839f1bb2
Luke Olali <mugapedia@gmail.com>
2020-12-16 10:07:25 +0300

    defines the mock sagaas used to test out the handling of side effects dispatched to the store

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,17 +1,48 @@
-import { takeEvery, put } from 'redux-saga/effects'
+import { takeEvery } from 'redux-saga/effects'
 
+import { IMAGES } from '../constants'
 
 // worker saga
-function* workerSaga() {
-  console.log("WORKER SAGA HAS RUNNED")
-  yield put({ type: 'SAGA TEST '})
-  console.log(put({ type: 'FINAL ACTION FROM WORKER' }))
-  yield put({ type: 'FINAL ACTION FROM WORKER' })
+
+// function* workerSaga() {
+//   console.log("WORKER SAGA HAS RUNNED")
+//   yield put({ type: 'SAGA TEST '})
+//   console.log(put({ type: 'FINAL ACTION FROM WORKER' }))
+//   yield put({ type: 'FINAL ACTION FROM WORKER' })
+// }
+
+function* handleImagesLoad() {
+  console.log("fetching images from unsplash...")
 }
 
+// function* handleChant() {
+//   console.log('SAGA! SAGA!! SAGA!!!')
+// }
+
+// function* byebyeSaga() {
+//   console.log("bye bye, talk later")
+// }
+
 // watcher saga
+
+// function* rootSaga() {
+//   yield take('LOGIN')
+//   yield call(workerSaga)
+//   // yield take('ADD_TO_CART')
+//   // yield take('BUY')
+//   yield take('LOGOUT')
+//   yield call(byebyeSaga)
+// }
+
 function* rootSaga() {
-  yield takeEvery('HELLO', workerSaga)
+  yield takeEvery(IMAGES.LOAD, handleImagesLoad)
+  // yield takeEvery('CHANT', handleChant)
+  // yield take(IMAGES.LOAD)
+  // yield call(handleImagesLoad)
+  // yield take('CHANT')
+  // yield call(handleChant)
+  // yield take(IMAGES.LOAD)
+  // yield call(handleImagesLoad)
 }
 
 export default rootSaga
