24e2eac013162440c98e2bee1355b0775e0d26f4
Luke Olali <mugapedia@gmail.com>
2020-12-16 16:48:18 +0300

    changes the number of images to load per page

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
-const KEY = "?client_id=biUxdd_dhgL76DVTq3Nn8U3jUqx7DMjE4mlrt2wI10o|||||||||||||||"
+const KEY = "?client_id=biUxdd_dhgL76DVTq3Nn8U3jUqx7DMjE4mlrt2wI10o"
 const URL = "https://api.unsplash.com/photos/"
 
 const fetchImages = async page => {
-  const response = await fetch(`${URL}${KEY}&per_page=36&page=${page}`)
+  const response = await fetch(`${URL}${KEY}&per_page=6&page=${page}`)
   const data = response.json()
   if(response.status >= 400) {
     throw new Error(data.errors)
