770c544331b66007126349cc446302211ccb8565
Luke Olali <mugapedia@gmail.com>
2020-12-16 10:03:07 +0300

    adds a new property to the state variable that will be updated using the pageReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import loadingReducer from './loading_reducer'
 import imagesReducer from './images_reducer'
 import errorReducer from './error_reducer'
+import pageReducer from './page_reducer'
 
 const rootReducer = combineReducers({
   loading: loadingReducer,
   images: imagesReducer,
-  error: errorReducer
+  error: errorReducer,
+  nextPage: pageReducer
 })
 
 export default rootReducer
