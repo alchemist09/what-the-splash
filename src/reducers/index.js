16e0826003017e6c08c8d7c5dadb3dace314998b
Luke Olali <mugapedia@gmail.com>
2021-02-15 13:27:32 +0300

    Adds another field to the app state object to hold image statistics

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import loadingReducer from './loading_reducer'
 import imagesReducer from './images_reducer'
 import errorReducer from './error_reducer'
 import pageReducer from './page_reducer'
+import stateReducer from './stats_reducer'
 
 const rootReducer = combineReducers({
   loading: loadingReducer,
   images: imagesReducer,
   error: errorReducer,
-  nextPage: pageReducer
+  nextPage: pageReducer,
+  stats: stateReducer
 })
 
 export default rootReducer
