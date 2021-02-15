6257837949aac7185d0a58d8cc2d19800f3f9ca2
Luke Olali <mugapedia@gmail.com>
2021-02-15 13:42:20 +0300

    defines a worker saga for handling image stats load requests from UnSplash API

--- src/sagas/stats_saga.js
+++ src/sagas/stats_saga.js
@@ -3,6 +3,15 @@ import { setStats, setErrorStats } from '../actions'
 import { STATS } from '../constants'
 import { fetchStats } from '../api'
 
+function* handleStatsLoad() {
+  try {
+    const image_stats = yield call(fetchStats)
+    yield put(setStats(image_stats))
+  } catch (error) {
+    yield put(setErrorStats(error.toString()))
+  }
+}
+
 function* watchStatsLoad() {
   yield takeEvery(STATS.LOAD, handleStatsLoad)
 }
