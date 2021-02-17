b7881e2f2b64749cbbff30ff5ee6c6ac06c2f851
Luke Olali <mugapedia@gmail.com>
2021-02-17 15:48:34 +0300

    defines the root saga that has multiple sub sags

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,4 +1,11 @@
+import { fork } from 'redux-saga/effects'
+
 import imagesSaga from './images_saga'
+import statsSaga from './stats_saga'
 
+function* rootSaga() {
+  yield fork(imagesSaga)
+  yield fork(statsSaga)
+}
 
-export default imagesSaga
+export default rootSaga
