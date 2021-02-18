a09575e0ad6eff6ee3d5d67d0cc16a45777729c7
Luke Olali <mugapedia@gmail.com>
2021-02-18 17:22:02 +0300

    adds changes made on the Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 import './styles.css'
 
-const Button = ({ children, loading, ...props }) => (
+const Button = ({ children, loading, ...props }) => {
+  // console.log("props children: ", children)
+  return (
   <button className="button" disabled={loading} {...props}>
     {loading ? 'Loading.....' : children}
   </button>
-)
+  )
+}
 
 Button.defaultProps = {
   loading: false
