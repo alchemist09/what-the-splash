969b432a3f0b0e4c318471a3b63925b0eab2a37e
Luke Olali <mugapedia@gmail.com>
2021-02-18 16:36:51 +0300

    refactors the loading reducer to take into account the various states of loading image stats

--- src/reducers/loading_reducer.js
+++ src/reducers/loading_reducer.js
@@ -1,12 +1,15 @@
-import { IMAGES } from '../constants'
+import { IMAGES, STATS } from '../constants'
 
 const loadingReducer = (state = false, action) => {
   switch(action.type) {
     case IMAGES.LOAD:
+    case STATS.LOAD:
       return true
     case IMAGES.LOAD_SUCCESS:
+    case STATS.LOAD_SUCCESS:
       return false
     case IMAGES.LOAD_FAIL:
+    case STATS.LOAD_FAIL:
       return false
     default:
       return state
