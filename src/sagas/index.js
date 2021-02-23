3706defc99f7ce7d006b4f50abb9ac29166baed1
Luke Olali <mugapedia@gmail.com>
2021-02-23 21:03:19 +0300

    adds imageStatsSaga to the root saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,11 +2,13 @@ import { fork, all } from 'redux-saga/effects'
 
 import imagesSaga from './images_saga'
 import statsSaga from './stats_saga'
+import imageStatsSaga from './image_stats_saga'
 
 function* rootSaga() {
   yield all([
     fork(imagesSaga),
-    fork(statsSaga)
+    fork(statsSaga),
+    fork(imageStatsSaga)
   ])
 }
 
