e39e7e02b9589321a9dd35f94cdb871855c05a76
Luke Olali <mugapedia@gmail.com>
2021-02-23 21:29:18 +0300

    defines the body of the handleImageStatsLoad() task

--- src/sagas/image_stats_saga.js
+++ src/sagas/image_stats_saga.js
@@ -1,8 +1,24 @@
 import { IMAGES } from '../constants'
-import { take, fork } from 'redux-saga/effects'
+import { take, fork, call, put } from 'redux-saga/effects'
+import { fetchImageStats } from '../api'
+import { setImageStats, setImageStatsError} from '../actions'
 
 function* handleImageStatsLoad(id) {
   console.log(`handle stats request for image ${id}`)
+  try {
+    const image_stats = yield call(fetchImageStats, id)
+    yield put(setImageStats({
+      id,
+      downloads: image_stats.downloads.total,
+      likes: image_stats.likes.total,
+      views: image_stats.views.total
+    }))
+  } catch (error) {
+    yield put(setImageStatsError({
+      id,
+      error
+    }))
+  }
 }
 
 function* watchImageStatsLoad() {
