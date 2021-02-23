35438b9edb9ce9e60d47ce8fd2182d16b7e0b9b9
Luke Olali <mugapedia@gmail.com>
2021-02-23 21:24:42 +0300

    renames the action creators for handling sitewide statistics to match those defined within actions.js

--- src/sagas/stats_saga.js
+++ src/sagas/stats_saga.js
@@ -1,14 +1,14 @@
 import { call, takeEvery, put } from 'redux-saga/effects'
-import { setImageStats, setImageStatsError } from '../actions'
+import { setSiteStats, setSiteStatsError } from '../actions'
 import { STATS } from '../constants'
 import { fetchStats } from '../api'
 
 function* handleStatsLoad() {
   try {
     const image_stats = yield call(fetchStats)
-    yield put(setImageStats(image_stats))
+    yield put(setSiteStats(image_stats))
   } catch (error) {
-    yield put(setImageStatsError(error.toString()))
+    yield put(setSiteStatsError(error.toString()))
   }
 }
 
