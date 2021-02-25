c3cc639540c8a64073b98e73fbfa041be15093ad
Luke Olali <mugapedia@gmail.com>
2021-02-25 13:50:12 +0300

    defines the Stats component to display statistics of a single image

---
+++ src/components/Stats/Stats.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+
+const Stats = ({ stats, id }) => {
+  if(Object.keys(stats).length === 0) {
+    return '<span>Loading......</span>'
+  }
+
+  console.log("Stats Prop", stats)
+  console.log("Stats ID: ", id)
+  // const { downloads, views, likes, error } = stats.payload
+  return (
+    <Fragment>
+      <span>{stats[id] && stats[id].error && 'Error!!!'}</span>
+      <span>{stats[id] && stats[id].loading && 'Loading.....'}</span>
+      <span>{stats[id] && !stats[id].loading && 'Downlaods: ' + stats[id].payload.downloads}&nbsp;&nbsp;&nbsp;</span>
+      <span>{stats[id] && !stats[id].loading && 'Views: ' + stats[id].payload.views}&nbsp;&nbsp;&nbsp;</span>
+      <span>{stats[id] && !stats[id].loading && 'Likes: ' + stats[id].payload.likes}</span>
+    </Fragment>
+  )
+}
+
+export default Stats
