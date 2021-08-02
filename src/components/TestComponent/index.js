a4a50cb640d87a424a6e50a7ce6e34a3485ee940
Luke Olali <mugapedia@gmail.com>
2021-08-02 23:57:01 +0300

    declares the TestComponent

---
+++ src/components/TestComponent/index.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import icon_downalod from '../../assets/icons/icon-download.png'
+import './styles.css'
+
+const TestComponent = () => {
+  return (
+    <Fragment>
+      153288
+      <img className="icon-download" src={icon_downalod} alt="download icon" />
+    </Fragment>
+    
+  )
+}
+
+export default TestComponent
