302973caed031d9da42f5abc6df2d816544d5ff6
Luke Olali <mugapedia@gmail.com>
2021-08-18 15:05:27 +0300

    tests successful loading of images

--- src/sagas/__tests__/images_saga.spec.js
+++ src/sagas/__tests__/images_saga.spec.js
@@ -1,4 +1,7 @@
-import { getPage } from "../images_saga";
+import { getPage, handleImagesLoad } from "../images_saga";
+import { runSaga } from 'redux-saga'
+import { fetchImages } from '../../api'
+import { setImages } from "../../actions";
 
 describe("Images Saga", () => {
   test("should give us the next page", () => {
@@ -6,5 +9,23 @@ describe("Images Saga", () => {
     const state = { nextPage }
     const res = getPage(state)
     expect(res).toBe(nextPage)
+  })
+
+  test("should load images and handle them in case of success", async () => {
+    const dispatchedActions = []
+    const fakeStore = {
+      dispatch: action => dispatchedActions.push(action),
+      getState: () => ({ nextPage: 1 })
+    }
+
+    const page_no = 1
+    const images = await fetchImages(page_no)
+
+    await runSaga(fakeStore, handleImagesLoad).done
+    console.log(dispatchedActions)
+    console.log(images)
+    console.log(fakeStore.getState())
+    
+    expect(dispatchedActions).toContainEqual(setImages(images))
   })
 })
