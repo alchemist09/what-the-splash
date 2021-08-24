489352c22791334d0391ada132af8c7ba03b33d3
Luke Olali <mugapedia@gmail.com>
2021-08-24 16:11:16 +0300

    sets up the test suite for testing image statistics

---
+++ src/sagas/__tests__/image_stats_saga.spec.js
@@ -0,0 +1,10 @@
+import { runSaga } from 'redux-saga'
+import { handleImageStatsLoad } from '../image_stats_saga'
+import * as api from '../../api'
+import { setImageStats } from '../../actions'
+
+describe("Image Stats Saga", () => {
+  test("should load statistics for a particular image and handle them if successful", () => {
+
+  })
+})
