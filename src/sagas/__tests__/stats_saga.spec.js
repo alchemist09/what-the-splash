06d1261ac8e82d1c059be0800581c4c3299cbc1b
Luke Olali <mugapedia@gmail.com>
2021-08-24 14:55:41 +0300

    tests loading of overall site statistics

--- src/sagas/__tests__/stats_saga.spec.js
+++ src/sagas/__tests__/stats_saga.spec.js
@@ -1,10 +1,22 @@
 import { handleStatsLoad } from '../stats_saga'
 import { runSaga } from 'redux-saga'
 import * as api from '../../api'
+import { setSiteStats } from '../../actions'
 
 
 describe('Stats Saga', () => {
-  test('should load general statistics and handle them if successful', () => {
-    
+  test('should load general statistics and handle them if successful', async () => {
+    const dispatchedActions =  []
+    const fakeStore = {
+      dispatch: action => dispatchedActions.push(action),
+      getState: () => {}
+    }
+
+    const site_stats = await api.fetchStats()
+    await runSaga(fakeStore, handleStatsLoad).done
+    console.log(dispatchedActions)
+    console.log(site_stats)
+
+    expect(dispatchedActions).toContainEqual(setSiteStats(site_stats))
   })
 })
