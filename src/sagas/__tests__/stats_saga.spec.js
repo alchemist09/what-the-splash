b4ffe2b276756d98c7bf0771dc0bb4f5862df2b8
Luke Olali <mugapedia@gmail.com>
2021-08-24 14:34:28 +0300

    sets up testing for the stats saga

---
+++ src/sagas/__tests__/stats_saga.spec.js
@@ -0,0 +1,10 @@
+import { handleStatsLoad } from '../stats_saga'
+import { runSaga } from 'redux-saga'
+import * as api from '../../api'
+
+
+describe('Stats Saga', () => {
+  test('should load general statistics and handle them if successful', () => {
+    
+  })
+})
