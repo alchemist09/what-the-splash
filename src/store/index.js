2fd3630bbd3226682bd4cc17579f5546f9305915
Luke Olali <mugapedia@gmail.com>
2021-08-31 23:29:41 +0300

    removes commented out code and console.log statements

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import createSageMiddleware from 'redux-saga'
-// import { IMAGES } from '../constants'
 
 import rootReducer from '../reducers'
 import rootSaga from '../sagas'
@@ -16,20 +15,6 @@ const configureStore = () => {
   )
 
   const taskObject = sagaMiddleware.run(rootSaga)
-  console.log("TASK OBJECT")
-  console.log(taskObject)
-  console.log("isRunning(): ",  taskObject.isRunning())
-  // taskObject.cancel()
-  // console.log("isRunning() after canceling: ",  taskObject.isRunning())
-  console.log("result(): ", taskObject.result())
-  // setTimeout(() => {
-  //   console.log("isRunning() after 30 seconds: ", taskObject.isRunning())
-  // }, 30000)
-  // store.dispatch({ type: 'LOGOUT' })
-  // store.dispatch({ type: 'LOGIN' })
-  // store.dispatch({ type: 'LOGOUT' })
-  // store.dispatch({ type: IMAGES.LOAD })
-  // store.dispatch({ type: 'CHANT' })
   return store
 }
 
