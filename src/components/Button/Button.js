437253dbe574d277352743c7c38d08e5943a31e6
Luke Olali <mugapedia@gmail.com>
2021-08-31 23:34:42 +0300

    removes commented out code from Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,6 @@ import React from 'react'
 import './styles.css'
 
 const Button = ({ children, loading, ...props }) => {
-  // console.log("props children: ", children)
   return (
   <button className="button" disabled={loading} {...props}>
     {loading ? 'Loading.....' : children}
